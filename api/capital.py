9c8fb58177aac45bb0f4f15616aa4a8ae14c26e0
DANNER TAYLOR <DANNER_TN@HOTMAIL.COM>
2023-09-11 00:32:03 -0500

    fix code to reach request

--- api/capital.py
+++ api/capital.py
@@ -1,38 +1,45 @@
 from http.server import BaseHTTPRequestHandler
+from urllib import parse
 import requests
-import urllib.parse
 
 
 class handler(BaseHTTPRequestHandler):
-
     def do_GET(self):
-        # Parse the query parameters
-        query = urllib.parse.urlparse(self.path).query
-        query_params = urllib.parse.parse_qs(query)
+        # Parse the URL to get the query parameters
+        url_components = parse.urlsplit(self.path)
+        query_params = dict(parse.parse_qsl(url_components.query))
 
-        # Interact with the REST Countries API
-        response = requests.get("https://restcountries.com/v3.1/capital")
-        countries = response.json()
+        # Base URL for the REST Countries API
+        base_api_url = "https://restcountries.com/v3.1/"
 
-        # Find the country or capital based on the query
-        message = "Not found"
-        if "country" in query_params:
-            country_name = query_params["country"][0].capitalize()
-            for country in countries:
-                if country_name in country["name"]["common"]:
-                    capital = country["capital"][0]
-                    message = f"The capital of {country_name} is {capital}"
-                    break
-        elif "capital" in query_params:
-            capital_name = query_params["capital"][0]
-            for country in countries:
-                if capital_name in country["capital"]:
-                    country_name = country["name"]["common"]
-                    message = f"{capital_name} is the capital of {country_name}"
-                    break
+        # Check if 'country' is in the query parameters
+        if 'country' in query_params:
+            country_name = query_params['country']
+            response = requests.get(base_api_url + "name/" + country_name)
+            if response.status_code == 200:
+                data = response.json()
+                capital = data[0]['capital'][0]
+                message = f"The capital of {country_name.title()} is {capital}."
+            else:
+                message = f"Country named {country_name.title()} not found."
+
+        # Check if 'capital' is in the query parameters
+        elif 'capital' in query_params:
+            capital_name = query_params['capital']
+            response = requests.get(base_api_url + "capital/" + capital_name)
+            if response.status_code == 200:
+                data = response.json()
+                country = data[0]['name']['common']
+                message = f"{capital_name.title()} is the capital of {country}."
+            else:
+                message = f"Capital named {capital_name.title()} not found."
+
+        else:
+            message = "Please provide a valid 'country' or 'capital' in the query parameters."
 
         # Send the response
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
         self.wfile.write(message.encode())
+
